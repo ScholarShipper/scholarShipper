97ae8d4e3f680f25bfe4a9306374b3c97ea67b72
Todd Alexander <60899614+toddalex@users.noreply.github.com>
2020-06-03 10:57:14 -0700

    configuring db connection

--- scholarshipper/server/server.ts
+++ scholarshipper/server/server.ts
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const studentControllers = require('./controllers/studentcontroller.ts')
 
 const app = express();
 app.use(express.json());
@@ -12,13 +13,28 @@ app.get('/', (req, res) => {
 });
 
 // Serve static assets:
-app.use('/src/assets',
-  express.static(path.resolve(__dirname, '../src/assets'))
-);
+// app.use('/src/assets',
+//   express.static(path.resolve(__dirname, '../src/assets'))
+// );
 
 app.get('/test', (req, res) => {
   console.log('request works')
   res => res.status(200).send('here are the results...')
+});
+
+app.get('/students', studentControllers.getStudents, (req, res) => {
+  res.json(res.locals.students)
+})
+
+// catches bad routes
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Server Error');
 });
 
 app.listen(3000, () => 
