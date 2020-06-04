7fa5f1153d96e37d491bd24cf8c8d6d28123db46
ajlee12 <rach.on@gmail.com>
2020-06-04 15:39:49 -0700

    Reconcile new UI elements from Brian.

--- scholarshipper/src/App.tsx
+++ scholarshipper/src/App.tsx
@@ -9,38 +9,35 @@ const { ipcRenderer } = window.require('electron');
 // import NavBar from './components/NavBar.jsx';
 
 function App(props) {
-  const [cohortData, setCohortData] = useState([]);
+  // const [cohortData, setCohortData] = useState([]);
 
-  useEffect(() => {
-    // Initiate renderer process to ipcMain to query DB for cohort data.
-    ipcRenderer.send('getAllCohorts');
-    // Catch the data sent back by ipcMain.
-    ipcRenderer.on('gotAllCohorts', (event, cohortData) => {
-      setCohortData([...cohortData]);
-    })
-  }, []);
+  // useEffect(() => {
+  //   // Initiate renderer process to ipcMain to query DB for cohort data.
+  //   ipcRenderer.send('getAllCohorts');
+  //   // Catch the data sent back by ipcMain.
+  //   ipcRenderer.on('gotAllCohorts', (event, cohortData) => {
+  //     setCohortData([...cohortData]);
+  //   })
+  // }, []);
 
-  console.log('this is cohortData in App:',cohortData)
-  
+  // console.log('this is cohortData in App:',cohortData)
+
   return (
     <div className="App">
-      <div className="bar">
-        <a className="logo">
-          <img src={logo} alt="Logo" height='150' width='150'></img>
-        </a>
-      </div>
-      <div>
-        <br></br>
-        <LinkContainer className="login" to="/Cohort">
-          <button className="btn btn-info btn-lg">Admin Login</button>
-        </LinkContainer>
-      </div>
-      <Grid cohortData={cohortData} />
-      <br></br>
-      <div className="bar">
-        <div className="helloText">ScholarShipper</div>
-        <div className="helloTextSmall">...to college and beyond</div>
-      </div>
+        <div className="bar">
+          <div className="helloText">ScholarShipper</div>
+          <div className="helloTextSmall">...to college and beyond</div>
+          <a className="logo">
+            <img src={logo} alt="Logo" height='150' width='150'></img>
+          </a>
+        </div>
+
+        <div>
+          <br></br>
+          <LinkContainer className="login" to="/Cohort">
+            <button className="btn btn-info btn-lg">Admin Login</button>
+          </LinkContainer>
+        </div>
     </div>
   );
 } 
