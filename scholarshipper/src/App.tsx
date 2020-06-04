426df5477d53d205ac54f1dd494a60d9e27ebecd
Todd Alexander <60899614+toddalex@users.noreply.github.com>
2020-06-04 14:54:04 -0700

    adding db queries for cohorts

--- scholarshipper/src/App.tsx
+++ scholarshipper/src/App.tsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './assets/ScholarShipperIcon_.png';
 import Grid from './components/Grid.jsx';
 import { LinkContainer } from 'react-router-bootstrap';
-import Student from './Student'
-// const electron = require('electron')
-// const path = require('path')
+import Student from './Student';
+const { ipcRenderer } = window.require('electron');
 // const BrowserWindow = electron.remote.BrowserWindow
 // import NavBar from './components/NavBar.jsx';
 
-function App() {
+function App(props) {
+  const [cohortData, setCohortData] = useState([]);
+
+  useEffect(() => {
+    // Initiate renderer process to ipcMain to query DB for cohort data.
+    ipcRenderer.send('getAllCohorts');
+    // Catch the data sent back by ipcMain.
+    ipcRenderer.on('gotAllCohorts', (event, cohortData) => {
+      setCohortData([...cohortData]);
+    })
+  }, []);
+  console.log('this is cohortData in App:',cohortData)
   return (
     <div className="App">
-        <div className="bar">
-          <a className="logo">
-            <img src={logo} alt="Logo" height='80' width='80'></img>
-          </a>
-        </div>
-        <div>
+      <div className="bar">
+        <a className="logo">
+          <img src={logo} alt="Logo" height='80' width='80'></img>
+        </a>
+      </div>
+      <div>
         <br></br>
         {/* <LinkContainer to="/Student">
           <button className="btn btn-info btn-lg">Student Form</button>
         </LinkContainer> */}
-        </div>
-        <Grid />
-        {/* <Student /> */}
+      </div>
+      <Grid cohortData={cohortData} />
+      {/* <Student /> */}
     </div>
   );
 } 
--- scholarshipper/src/components/Card.jsx
+++ scholarshipper/src/components/Card.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
+import Student from '../Student'
+const { ipcRenderer } = window.require('electron');
 
-const Cards = () => {
+const Card = (props) => {
+  // props.cohortData contains cohort_id and start_year
+  const { cohortData } = props;
+  // const [students, setStudents] = useState([]);
+
+  // useEffect(() => {
+  //   // Initiate renderer process to ipcMain to query DB for cohort data.
+  //   ipcRenderer.send('getstudentCohort',['']);
+  //   // Catch the data sent back by ipcMain.
+  //   ipcRenderer.on('gotStudentCohorts', (event, cohortData) => {
+  //     setStudents([...students]);
+  //   })
+  // }, []);
+
   return (
-    <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Cohort</h5>
-          <p class="card-text"># goes here</p>
-          <LinkContainer to="/Student">
-            <a class="btn btn-primary btn-sm btn-info" href="#" role="button" onClick="studentPopUp()">Link</a>
-          </LinkContainer>
-        </div>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Cohort</h5>
+        <LinkContainer to={`/Student/${cohortData.cohort_id}`}>
+          <a className="card-text">{cohortData.start_year}</a>
+        </LinkContainer>
       </div>
+    </div>
   )
 }
 
-export default Cards;
+export default Card;
